245d62830255d392d54e02aa75d32fca22d29341
Patrick Bentley <pbentley07@gmail.com>
2018-05-22 14:55:08 +0300

    Fixes Async promis problem with db.Create.Applicants. Mailer now functions

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -21,17 +21,7 @@ module.exports = function(app) {
       res.json(appPost);
     });
   });
-  // Get route for returning posts of a specific category
-//   app.get("/api/posts/category/:category", function(req, res) {
-//     db.Post.findAll({
-//       where: {
-//         category: req.params.category
-//       }
-//     })
-//     .then(function(dbPost) {
-//       res.json(dbPost);
-//     });
-//   });
+
   // Get rotue for retrieving a single post
 //   app.get("/api/posts/:id", function(req, res) {
 //     db.Post.findOne({
@@ -44,32 +34,35 @@ module.exports = function(app) {
 //     });
 //   });
   // POST route for saving a new post
-    app.post("/api/applicants", function(req, res) {
-      db.Applicant.create(req.body);
-      res.send('Successfully posted new Applicant');       
-      }).then(function(){
-          //mail details for nodemailer
-          let mailOptions = {
-              from: '"noreply@cap.org" <app@cap.org>', // sender address
-              to: 'ijvv7dth54f7zp3w@ethereal.email', // list of receivers
-              subject: 'Application Submitted', // Subject line
-              text: req.body.firstname + ' ' + req.body.last_name + ' just sent you a message!', // plain text body
-              html: '<b>'+req.body.first_name+'</b>' + '</br>' +
-              ''  + req.body.last_name   + '</br>' + 
-              'DOB: ' 
-               // html body
-          };
-          transporter.sendMail(mailOptions, (error, info) => {
-              if (error) {
-                  return console.log(error);
-              }
-              console.log('Message sent: %s', info.messageId);
-              // Preview only available when sending through an Ethereal account
-              console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-              // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-              // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-          });
+  app.post("/api/applicants", function(req, res) {
+    return db.Applicant.create(req.body).then(function(appPost){
+      // Respond to the HTTP request
+      res.send('Successfully posted new Applicant');
+      res.end(); // Ensure the response is sent before any emailing is attempted
+  
+      //mail details for nodemailer
+      let mailOptions = {
+        from: '"noreply@cap.org" <app@cap.org>', // sender address
+        to: 'ijvv7dth54f7zp3w@ethereal.email', // list of receivers
+        subject: 'Application Submitted', // Subject line
+        text: req.body.firstname + ' ' + req.body.last_name + ' just sent you a message!', // plain text body
+        html: '<b>'+req.body.first_name+'</b>' + '</br>' +
+        ''  + req.body.last_name   + '</br>' +
+        'DOB: '
+        // html body
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
       });
+    });
+  });
       
     // DELETE route for deleting posts
   //   app.delete("/api/posts/:id", function (req, res) {
@@ -95,5 +88,6 @@ module.exports = function(app) {
   //       res.json(dbPost);
   //     });
   //   });
-}
+        
+};
 
